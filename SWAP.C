8b83103d06192f4dfd790de10fcf289b3e044c88
AnusreeK-2000 <53806481+AnusreeK-2000@users.noreply.github.com>
2019-08-09 19:39:56 +0530

    Add files via upload

---
+++ SWAP.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void swap(int *,int *);
+int main()
+{
+	int a,b;
+	printf("enter a and b values");
+	scanf("%d %d",&a,&b);
+	printf("\n the values of a and b before swapping are %d %d",a,b);
+	swap(&a,&b);
+	printf("\n the values of a and b after swapping are %d %d",a,b);
+	getch();
+	return 0;
+}
+void swap(int *x,int *y)
+{
+	int t=0;
+	t=*x;
+	*x=*y;
+	*y=t;
+}
